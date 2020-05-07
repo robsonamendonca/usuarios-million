af8a822874ef324a8c2101fc387833ca64758d78
Robson Mendonça <rob.ams@gmail.com>
2020-05-07 19:22:55 -0300

    configurando variaveis de ambiente para deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
-require('dotenv').config({
-    path: '.env'
-})
+// require('dotenv').config({
+//     path: '.env'
+// })
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/millionUsuarios';
 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
